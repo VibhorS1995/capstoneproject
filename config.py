deb5609f3306aef787256ad3ea297bcc3cbcdf32
vibhors1995 <shrivastava.vibhor22@gmail.com>
2023-04-15 12:25:05 +0000

    first commit

---
+++ config.py
@@ -0,0 +1,8 @@
+custombucket = "s3-capstone-project"
+table = "intellipaat"
+databasehost = "database-1.cgwnjgqq2n7n.us-west-2.rds.amazonaws.com"
+duser = "admin"
+dpass = "admin123"
+s3database = "intellipaat"
+kapp = "http://192.168.3.174:32486"
+
